5e231802398e7907bda4b3c1ef1c6db669e7762b
Anthony Garand <garand@garand.me>
2017-03-07 14:05:11 -0500

    Update start script to use native docker port command

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
-export DEV_PORT=\"$(docker inspect --format='{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $(docker-compose ps -q) )\"
+export DEV_PORT=\"$(docker-compose port web 80 |  sed 's/.*://')\"
 webpack --watch
