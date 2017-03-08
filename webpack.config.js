b9a64b267a0b3d159f177418ce7638a64a413815
Christian Chung <christian.m.chung@gmail.com>
2017-03-08 15:43:10 -0500

    app(*): Initial commit of react and php

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const proxyHost = 'http://0.0.0.0:';
-const proxyPort = process.env.DEV_PORT.replace(/"/g, '');
+const proxyPort = process.env.DEV_PORT;
 
 module.exports = {
 	context: path.resolve(__dirname, 'app'),
 	entry: {
-		app: './app.js'
+		main: [ 'babel-polyfill', 'whatwg-fetch', './app.js'],
+		admin: [ 'babel-polyfill', 'whatwg-fetch', './admin/admin-main.js' ],
 	},
 	module: {
 		loaders: [
@@ -18,19 +19,43 @@ module.exports = {
 					loader: 'css-loader?importLoaders=1!postcss-loader'
 				}),
 			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: [
+						[ "es2015", { modules: false } ],
+						"stage-0",
+						"react"
+					],
+					plugins: [
+						'transform-async-to-generator',
+						'transform-decorators-legacy'
+					]
+				}
+			}
 		],
 	},
 	output: {
-		path: path.resolve(__dirname, './app'),
-		filename: 'bundle.js'
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js'
 	},
 	plugins: [
 		new ExtractTextPlugin('bundle.css'),
+		//new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.CommonsChunkPlugin({
+			names: [ 'vendor', 'manifest' ]
+		}),
 		new BrowserSyncPlugin({
 			host: 'localhost',
 			port: 3000,
-			proxy: proxyHost.concat(proxyPort),
-			notify: false
+			proxy: proxyHost.concat(proxyPort.replace(/"/g, '')),
+			notify: false,
+			files: [
+				'./**/*.css',
+				'./app/bundle.js',
+				'./**/*.html'
+			]
 		})
 	],
 };
