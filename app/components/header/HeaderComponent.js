d732feff1ce364b8eea8e09d9e9acdb193ee7164
Christian Chung <christian.m.chung@gmail.com>
2017-03-09 16:52:24 -0500

    component(header): Add links to site and predictions tag

--- app/components/header/HeaderComponent.js
+++ app/components/header/HeaderComponent.js
@@ -5,9 +5,15 @@ export default class HeaderComponent extends Component {
 	render() {
 		return (
 			<div className="header">
+				<div className="header__section header__section--left">
+					<a className="header__link header__link--site" href="https://campusinsiders.com">Back to CampusInsiders.com</a>
+				</div>
 				<a className="header__logo" href="/">
-					<img src="https://campusinsiders.com/wp-content/themes/stadium/vendor/lift/playbook/dist/assets/components/common/assets/campus-insiders-logo.svg" />
+					<img src="https://campusinsiders.com/wp-content/themes/stadium/vendor/lift/playbook/dist/assets/components/common/assets/campus-insiders-logo.svg"/>
 				</a>
+				<div className="header__section header__section--right">
+					<a className="header__link header__link--news" href="https://campusinsiders.com/news/tag/ncaa-tournament-predictions/">NCAA Tourament News</a>
+				</div>
 			</div>
 		);
 	}
