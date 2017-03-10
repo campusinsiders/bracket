92fb7d275493d90c3d9288c5ea069eafa4546bc0
Christian Chung <christian.m.chung@gmail.com>
2017-03-10 03:35:49 -0500

    chore(webpack): Fix production build files

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
 	},
 	plugins: [
 		new ExtractTextPlugin('bundle.css'),
-		//new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.UglifyJsPlugin(),
 		new webpack.optimize.CommonsChunkPlugin({
 			names: [ 'vendor', 'manifest' ]
 		}),
