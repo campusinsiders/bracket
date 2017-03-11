c33b5e6ac6a86936f5e5f9965c5a5850c7ce7f59
Christian Chung <christian.m.chung@gmail.com>
2017-03-11 15:11:13 -0500

    component(region-toggle): Make it dynamic

--- app/components/region-toggle/RegionToggleComponent.js
+++ app/components/region-toggle/RegionToggleComponent.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
 
+@inject( 'store' ) @observer
 export default class RegionToggleComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.regionLabels = new Map();
+	}
+
+	setRegions() {
+		let regions = this.props.store.getRound('fourth');
+		if ( regions.length ) {
+			regions.map( (region) => {
+				let position = region.position.toUpperCase().slice(0,1).concat(region.position.slice(1));
+				this.regionLabels.set(`top${position}`, region.quadrantTop);
+				this.regionLabels.set(`bottom${position}`, region.quadrantBottom);
+			});
+		}
+		console.log( this.regionLabels );
+	}
+
+	getRegions() {
+		this.setRegions();
+		let regions = [];
+		for ( let [ key, value ] of this.regionLabels.entries() ) {
+			let className = 'regionToggle__region ';
+			className = ( 'topLeft' === key ) ? className.concat('regionToggle__region--active') : className;
+			regions.push(
+				<div key={key} data-region={key} className={className}>{value}</div>
+			);
+		}
+		regions.push( <div key="finalFour" data-region="finalFour" className="regionToggle__region">Final 4</div> );
+		return regions;
+	}
 
 	render() {
 		return (
 			<div className="regionToggle">
-				<div data-region="south" className="regionToggle__region regionToggle__region--active">South</div>
-				<div data-region="east" className="regionToggle__region">East</div>
-				<div data-region="west" className="regionToggle__region">West</div>
-				<div data-region="midwest" className="regionToggle__region">Midwest</div>
-				<div data-region="finalFour" className="regionToggle__region">Final 4</div>
+				{this.getRegions()}
 			</div>
 		);
 	}
